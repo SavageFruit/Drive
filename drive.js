430855a16137d652e8930513a3d2e04045a6a061
SavageFruit <70027644+SavageFruit@users.noreply.github.com>
2020-11-29 17:06:47 +0000

    Update drive.js

--- drive.js
+++ drive.js
@@ -1,6 +1,26 @@
 export class Drive {
   static Start() {
-    clear();
     console.log('Drive loaded');
+  }
+  static install(options) {
+    if(options.type === 'pkg') {
+      var package = document.createElement("script");
+      if(options.extension === true) {
+        package.src = options.packages.source + options.packages.extension;
+      } else if(options.extension === false) {
+        package.src = options.packages.source;
+      } else {
+        console.error('No valid Package Extension received');
+      }
+    } else if(options.type === 'script') {
+      var script = document.createElement("script");
+      if(options.extension === true) {
+        script.src = options.packages.source + options.packages.extension;
+      } else if(options.extension === false) {
+        script.src = options.packages.source;
+      } else {
+        console.log('No valid Package Extension received');
+      }
+    }
   }
 }
